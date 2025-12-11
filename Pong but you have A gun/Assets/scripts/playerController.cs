82391dbf3fe882e3cf6cf15ec1689c4b30d3fffe
Vultus Limunaria <danobrovold@gmail.com>
2025-12-11 23:09:45 +0100

    Add 2 Player and Fix Movement

--- Pong but you have A gun/Assets/scripts/playerController.cs
+++ Pong but you have A gun/Assets/scripts/playerController.cs
@@ -5,26 +5,45 @@ using UnityEngine.InputSystem;
 public class playerController : MonoBehaviour
 {
     InputAction move;
+    InputAction p2move;
     Rigidbody2D rb;
     public int speed;
+    public int plr;
     
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         move = InputSystem.actions.FindAction("move");
+        p2move = InputSystem.actions.FindAction("P2Move");
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 input = move.ReadValue<Vector2>();
-        rb.linearVelocity = new Vector2(0, input.y * speed);
-
-        if(transform.position.y > 3.5)
+        Vector2 input;
+        if(plr == 1)
         {
-            Debug.Log("what the fuck dude");
+            input = move.ReadValue<Vector2>();        
+        }
+        else
+        {
+            input = p2move.ReadValue<Vector2>();
+        }
+
+        if(transform.position.y > 3.5 && input.y > 0)
+        {  
+            rb.linearVelocity = new Vector2(0,0);
+        }
+        else if(transform.position.y < -3.5 && input.y < 0)
+        {
+            rb.linearVelocity = new Vector2(0,0);
+        }
+        else
+        {
+            rb.linearVelocity = new Vector2(0, input.y * speed);
+
         }
     }
 }
